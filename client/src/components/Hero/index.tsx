9dc0c6908d07c2c00927a96d5cc988a574cc4247
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-03-24 00:06:01 +0100

    template & homepage improvements

---
+++ client/src/components/Hero/index.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import styles from './styles.module.css';
+
+const Hero = () => {
+  return (
+    <>
+      <section
+        id="home"
+        className="relative z-10 overflow-hidden bg-custom-background bg-cover bg-center h-screen flex items-center justify-center"
+        style={{ backgroundImage: 'url("/images/hero/homepage-background.jpg")' }}
+      >
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mx-auto max-w-[800px] text-center">
+              <h1 className={`${styles.buffaloFont} mb-5 text-3xl leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight`}>
+                  Nuffalo Jill
+                </h1>
+                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                  
+                </p>
+                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  Something here.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
