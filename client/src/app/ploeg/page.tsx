8549bd336565c21aead06149302b4a11e55d6a1a
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-04-02 17:02:44 +0200

    2 Ploegfotos side by side on mobile

--- client/src/app/ploeg/page.tsx
+++ client/src/app/ploeg/page.tsx
@@ -19,42 +19,44 @@ export const metadata: Metadata = {
 
 const Ploeg = () => {
   return (
-    <>
-        <Hero
-            title = "Ontmoet de ploeg"
-            subtitle="Hier komt een super mega coole foto van ploeg"
-        />
+      <div
+          className="z-0 bg-[url(/images/home-content-wrapper/parchment.jpg)] bg-cover bg-center relative"
+      >
+          <Hero
+              title="Ontmoet de ploeg"
+              subtitle="Hier komt een super mega coole foto van ploeg"
+          />
 
-        <section className="pb-[100px] pt-[100px]">
-            <div className="container pb-[50px] pl-[10%] pr-[10%]">
-                <h2 className="pb-[40px] text-center">Kandidaat Groep 5</h2>
-                <div className="-mx-6 flex flex-wrap justify-center">
-                    {entriesPloegleden.filter(x => (x.post === "Kandidaat Groep 5")).map((singlePloeglid) => (
-                        <div
-                            key={singlePloeglid.id}
-                            className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3 pl-[2%] pr-[2%] pb-[30px]"
-                        >
-                            <SinglePloeglid ploeglid={singlePloeglid}/>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className="w-full bg-white h-0.5"></div> {/* White line */}
-            <div className="container pb-[50px] pt-[50px]">
-                <h2 className="pb-[40px] text-center">Digitaal</h2>
-                <div className="-mx-6 flex flex-wrap justify-center">
-                    {entriesPloegleden.filter(x => (x.post === "Digitaal")).map((singlePloeglid) => (
-                        <div
-                            key={singlePloeglid.id}
-                            className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/4 pl-[2%] pr-[2%] pb-[30px]"
-                        >
-                            <SinglePloeglid ploeglid={singlePloeglid}/>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </section>
-    </>
+          <section className="pb-[100px] pt-[100px]">
+              <div className="container pb-[50px] pl-[10%] pr-[10%]">
+                  <h2 className="pb-[40px] text-center">Kandidaat Groep 5</h2>
+                  <div className="-mx-6 flex flex-wrap justify-center">
+                      {entriesPloegleden.filter(x => (x.post === "Kandidaat Groep 5")).map((singlePloeglid) => (
+                          <div
+                              key={singlePloeglid.id}
+                              className="w-1/2 px-4 lg:w-1/2 xl:w-1/3 pl-[2%] pr-[2%] pb-[30px]"
+                          >
+                              <SinglePloeglid ploeglid={singlePloeglid}/>
+                          </div>
+                      ))}
+                  </div>
+              </div>
+              <div className="w-full bg-white h-0.5"></div>{/* White line */}
+              <div className="container pb-[50px] pt-[50px]">
+                  <h2 className="pb-[40px] text-center">Digitaal</h2>
+                  <div className="-mx-6 flex flex-wrap justify-center">
+                      {entriesPloegleden.filter(x => (x.post === "Digitaal")).map((singlePloeglid) => (
+                          <div
+                              key={singlePloeglid.id}
+                              className="w-1/2 px-4 lg:w-1/3 xl:w-1/4 pl-[2%] pr-[2%] pb-[30px]"
+                          >
+                              <SinglePloeglid ploeglid={singlePloeglid}/>
+                          </div>
+                      ))}
+                  </div>
+              </div>
+          </section>
+      </div>
   );
 };
 
