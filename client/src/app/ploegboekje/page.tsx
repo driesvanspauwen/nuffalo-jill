9548db44c9ab59b1c000481d29a194a463009074
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-04-07 12:25:12 +0200

    Nog steeds witte flits bij volgende pagina + krijg pdf niet geresized, i give up

--- client/src/app/ploegboekje/page.tsx
+++ client/src/app/ploegboekje/page.tsx
@@ -3,6 +3,7 @@
 import {pdfjs, Document, Page} from 'react-pdf'
 import PdfEmbed from "@/components/PdfEmbed";
 import React from "react";
+import PDFViewer from "@/components/PdfEmbed";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
@@ -12,25 +13,7 @@ const Ploegboekje = () => {
 
 
     return (
-      <div
-          className="z-0 bg-[url(/images/home-content-wrapper/parchment.jpg)] bg-cover bg-center relative"
-      >
-          <section className="pb-[120px] pt-[100px]">
-              <div className="container">
-                  <div className="w-full -mx-4 flex justify-center"> {/* Centering the content */}
-                      <div className="w-full px-4">
-                          <div>
-                              <div className="w-full flex justify-center"> {/* Centering the embed element */}
-                                  <PdfEmbed/>
-                              </div>
-                              <a className="flex md:hidden justify-center mb-5 text-center" href="/ploegboekje/Ploegboekje.pdf"><u>Download
-                                  het ploegboekje!</u></a>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-      </div>
+      <PDFViewer file="ploegboekje/DonLupo-compressed.pdf"/>
   );
 };
 
--- client/src/components/PdfEmbed/index.tsx
+++ client/src/components/PdfEmbed/index.tsx
@@ -1,68 +1,119 @@
-import React, {useState, useRef, useEffect, useCallback} from "react";
+// components/pdfviewer.tsx
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { useEffect, useRef, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function PdfEmbed(props) {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1); //setting 1 to show first page
-    const canvasRef = useRef(null);
+export default function PDFViewer(props: any) {
+    const [numPages, setNumPages] = useState<number>(0);
+    const [pageNumber, setPageNumber] = useState<number>(1); // start on first page
+    const [loading, setLoading] = useState(true);
+    const [pageWidth, setPageWidth] = useState(0);
 
-    useEffect(() => {
-        if (canvasRef.current) {
-            canvasRef.current.canvas.willReadFrequently = true;
-        }
-    }, []);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1);
+    function onDocumentLoadSuccess({
+                                       numPages: nextNumPages,
+                                   }: {
+        numPages: number;
+    }) {
+        setNumPages(nextNumPages);
     }
 
-    const changePage = useCallback(
-        (offset) => {
-            setPageNumber((prevPageNumber) => prevPageNumber + offset);
-        },
-        [setPageNumber]
-    );
-
-    function previousPage() {
-        changePage(-1);
+    function onPageLoadSuccess() {
+        setPageWidth(window.innerWidth);
+        setLoading(false);
     }
 
-    function nextPage() {
-        changePage(1);
+    const options = {
+        cMapUrl: "cmaps/",
+        cMapPacked: true,
+        standardFontDataUrl: "standard_fonts/",
+    };
+
+    // Go to next page
+    function goToNextPage() {
+        setPageNumber((prevPageNumber) => prevPageNumber + 1);
     }
 
-    const { pdf } = props;
+    function goToPreviousPage() {
+        setPageNumber((prevPageNumber) => prevPageNumber - 1);
+    }
 
-    const PDF =  (
+
+    return (
+        //            <Nav pageNumber={pageNumber} numPages={numPages} />
         <>
-            <Document
-                file="ploegboekje/DonLupo.pdf"
-                onLoadSuccess={onDocumentLoadSuccess}
+            <div
+                hidden={loading}
+                style={{ height: "calc(80vh)" }}
+                className="flex items-center mt-[80px] mb-[300px]"
             >
-                <Page pageNumber={pageNumber} canvasRef={canvasRef} />
-            </Document>
-            <div>
-                <p>
-                    Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-                </p>
-                <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-                    Previous
-                </button>
-                <button
-                    type="button"
-                    disabled={pageNumber >= numPages}
-                    onClick={nextPage}
+                <div
+                    className={`flex items-center justify-between w-full absolute z-10 px-2`}
                 >
-                    Next
-                </button>
+                    <button
+                        onClick={goToPreviousPage}
+                        disabled={pageNumber <= 1}
+                        className="relative h-[calc(100vh - 64px)] px-2 py-24 text-gray-400 hover:text-gray-50 focus:z-20"
+                    >
+                        <span className="sr-only">Previous</span>
+                        <ChevronLeftIcon className="h-10 w-10" aria-hidden="true" />
+                    </button>
+                    <button
+                        onClick={goToNextPage}
+                        disabled={pageNumber >= numPages!}
+                        className="relative h-[calc(100vh - 64px)] px-2 py-24 text-gray-400 hover:text-gray-50 focus:z-20"
+                    >
+                        <span className="sr-only">Next</span>
+                        <ChevronRightIcon className="h-10 w-10" aria-hidden="true" />
+                    </button>
+                </div>
+
+                <div className="h-full flex justify-center mx-auto">
+                    <Document
+                        file={props.file}
+                        onLoadSuccess={onDocumentLoadSuccess}
+                        options={options}
+                        renderMode="canvas"
+                    >
+                        <Page
+                            className=""
+                            key={pageNumber}
+                            pageNumber={pageNumber}
+                            renderAnnotationLayer={false}
+                            renderTextLayer={false}
+                            onLoadSuccess={onPageLoadSuccess}
+                            onRenderError={() => setLoading(false)}
+                            width={Math.max(pageWidth * 0.8, 390)}
+                        />
+                    </Document>
+                </div>
             </div>
         </>
     );
+}
 
-    return PDF;
+
+function Nav({pageNumber, numPages}: {pageNumber: number, numPages: number}) {
+    return (
+        <nav className="bg-black mt-[75px]">
+            <div className="mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex flex-shrink-0 items-center">
+                            <p className="text-2xl font-bold tracking-tighter text-white">
+                                Papermark
+                            </p>
+                        </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <div className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">
+                            <span>{pageNumber}</span>
+                            <span className="text-gray-400"> / {numPages}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
