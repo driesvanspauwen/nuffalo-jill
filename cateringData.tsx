5edf25b3c15a1a3e0b3296806fff53e5d53e8ce8
Tobias12399 <139650062+Tobias12399@users.noreply.github.com>
2024-04-07 17:58:31 +0200

    Add files via upload

---
+++ cateringData.tsx
@@ -0,0 +1,60 @@
+import { Catering, CateringElement } from "@/types/catering";
+
+const cateringData: Catering[] = [
+    {
+      id: 1,
+      date: "Zondag 31/15",
+      cateringelements: [
+        { id: 1, content: "scs", price: 25 },
+        { id: 2, content: "cvdfgb", price: 45 },
+        { id: 3, content: "cdv", price: 59 }
+      ]
+    },
+    {
+        id: 2,
+        date: "Zondag 31/15",
+        cateringelements: [
+          { id: 1, content: "scs", price: 20 },
+          { id: 2, content: "cvdfgb", price: 40 },
+          { id: 3, content: "cdv dslkdfjkfdjkfd dsdf", price: 50 }
+        ]
+    },
+    {
+        id: 3,
+        date: "Zondag 31/15",
+        cateringelements: [
+          { id: 1, content: "scs lekkerezddfdsvfvdrfveredf", price: 25 },
+          { id: 2, content: "cvdfgb", price: 45 },
+          { id: 3, content: "cdv", price: 59 }
+        ]
+    },
+    {
+        id: 4,
+        date: "Zondag 31/15",
+        cateringelements: [
+          { id: 1, content: "scs", price: 25 },
+          { id: 2, content: "cvdfgb", price: 45 },
+          { id: 3, content: "cdv", price: 59 }
+        ]
+    },
+    {
+        id: 5,
+        date: "Zondag 31/15",
+        cateringelements: [
+          { id: 1, content: "scs", price: 25 },
+          { id: 2, content: "cvdfgb", price: 45 },
+          { id: 3, content: "cdv", price: 59 }
+        ]
+    },
+    {
+        id: 6,
+        date: "Zondag 31/15",
+        cateringelements: [
+          { id: 1, content: "scs", price: 25 },
+          { id: 2, content: "cvdfgb", price: 45 },
+          { id: 3, content: "cdv", price: 59 }
+        ]
+    },
+];
+
+export default cateringData;
