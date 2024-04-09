6feb981c6e7bb5750d711ff191a2353811d46072
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-04-09 11:07:52 +0200

    redesign goals

--- client/src/components/Goals/SingleGoal.tsx
+++ client/src/components/Goals/SingleGoal.tsx
@@ -8,17 +8,12 @@ const SingleGoal = ({ goal }: { goal: Goal }) => {
   return (
     <div className="mb-16">
       <div className="wow fadeInUp" data-wow-delay=".15s">
-        <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-sky bg-opacity-10 text-primary">
-          {icon}
-        </div>
-        <h3 className="mb-5 text-xl lg:text-2xl font-bold text-black dark:text-white sm:text-3xl xl:text-4xl">
+        <h3 className="mb-5 text-xl lg:text-2xl font-bold text-brown-dark sm:text-3xl xl:text-4xl">
           {title}
         </h3>
-        {/* Render the subgoals */}
         <div className="mb-12 max-w-[570px] lg:mb-0">
           <div className="mx-[-12px] flex flex-wrap">
             <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-full">
-              {/* Map through the subgoals and render each one */}
               {subgoals.map((subgoal: Subgoal) => (
                 <SubGoalList key={subgoal.id} text={subgoal.title} />
               ))}
--- client/src/components/Goals/index.tsx
+++ client/src/components/Goals/index.tsx
@@ -8,14 +8,15 @@ const Goals = () => {
       <section className="py-16 md:py-20 lg:py-28">
         <div id="goals" className="container">
           <SectionTitle
-              title="Main Goals"
-              paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              title="WHAT WE STAND FOR"
+              paragraph=""
               center
               mb="30px"
+              titleColor="brown-dark"
           />
 
           {/* Center the grid of three columns and spread out over the whole width */}
-          <div className="flex justify-center w-full">
+          <div className="mt-20 flex justify-center w-full">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-14 w-full max-w-7xl">
               {goalsData.map((goal, index) => (
                   <div key={goal.id} className="flex justify-center">
