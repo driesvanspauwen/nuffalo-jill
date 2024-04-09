054564a62d6d1cfd902a57ce3a3c42b4b4792be9
MaartenHaine <maarten.haine@student.kuleuven.be>
2024-04-09 13:23:52 +0200

    fixed error page

--- client/src/app/ploegboekje/page.tsx
+++ client/src/app/ploegboekje/page.tsx
@@ -1,41 +1,41 @@
-'use client'
-
-import {pdfjs, Document, Page} from 'react-pdf'
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-
+"use client";
 
 const Ploegboekje = () => {
   return (
-      <div
-          className="z-0 bg-[url(/images/home-content-wrapper/parchment.jpg)] bg-cover bg-center relative"
-      >
-          <section className="pb-[120px] pt-[100px]">
-              <div className="container">
-                  <div className="w-full -mx-4 flex justify-center"> {/* Centering the content */}
-                      <div className="w-full px-4">
-                          <div>
-                              <div className="w-full flex justify-center"> {/* Centering the embed element */}
-                                  <embed
-                                      className="hidden md:block"
-                                      style={{
-                                          width: '1600px',
-                                          height: '1000px',
-                                      }}
-                                      type='application/pdf'
-                                      src="/ploegboekje/DonLupo.pdf"
-                                  />
-                              </div>
-                              <a className="flex md:hidden justify-center mb-5 text-center" href="/ploegboekje/Ploegboekje.pdf"><u>Download
-                                  het ploegboekje!</u></a>
-                          </div>
-                      </div>
-                  </div>
+    <div className="relative z-0 bg-[url(/images/home-content-wrapper/parchment.jpg)] bg-cover bg-center">
+      <section className="pb-[120px] pt-[100px]">
+        <div className="container">
+          <div className="-mx-4 flex w-full justify-center">
+            {" "}
+            {/* Centering the content */}
+            <div className="w-full px-4">
+              <div>
+                <div className="flex w-full justify-center">
+                  {" "}
+                  {/* Centering the embed element */}
+                  <embed
+                    className="hidden md:block"
+                    style={{
+                      width: "1600px",
+                      height: "1000px",
+                    }}
+                    type="application/pdf"
+                    src="/ploegboekje/DonLupo.pdf"
+                  />
+                </div>
+                <a
+                  className="mb-5 flex justify-center text-center md:hidden"
+                  href="/ploegboekje/Ploegboekje.pdf"
+                >
+                  <u>Download het ploegboekje!</u>
+                </a>
               </div>
-          </section>
-      </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
 export default Ploegboekje;
-
