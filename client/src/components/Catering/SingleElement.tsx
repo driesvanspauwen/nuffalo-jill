39ab98f780b5e8061d027353c746bd906d738a06
Tiddo Nees <tiddo.nees@gmail.com>
2024-04-16 15:55:13 +0200

    Catering make-over and added values

--- client/src/components/Catering/SingleElement.tsx
+++ client/src/components/Catering/SingleElement.tsx
@@ -1,6 +1,6 @@
 const SingleElement = ({ text, price }) => (
-    <p className="mb-5 flex items-center text-lg font-medium text-body-color">
-        <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
+    <p className="mb-5 flex items-center text-lg font-medium text-orange-100 tracking-wide">
+        <span className="subpixel-antialiased mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-amber-200">
         €{price}
         </span>
         {text}
--- client/src/components/Catering/cateringData.tsx
+++ client/src/components/Catering/cateringData.tsx
@@ -3,56 +3,47 @@ import { Catering, CateringElement } from "@/types/catering";
 const cateringData: Catering[] = [
     {
       id: 1,
-      date: "Zondag 31/15",
+      date: "Toog",
       cateringelements: [
-        { id: 1, content: "scs", price: 25 },
-        { id: 2, content: "cvdfgb", price: 45 },
-        { id: 3, content: "cdv", price: 59 }
+        { id: 1, content: "Frisdrank/Bier/Redbull", price: 1 },
+        { id: 2, content: "Karmelier/Watermelon Spritz", price: 1.50 },
+        { id: 3, content: "Aperol Spritz", price: 2.50 }
       ]
     },
     {
         id: 2,
-        date: "Zondag 31/15",
+        date: "Frituur",
         cateringelements: [
-          { id: 1, content: "scs", price: 20 },
-          { id: 2, content: "cvdfgb", price: 40 },
-          { id: 3, content: "cdv dslkdfjkfdjkfd dsdf", price: 50 }
+          { id: 1, content: "Frietjes", price: 1.2 },
+          { id: 2, content: "Vlees/Kaaskroket", price: 1.2 },
+          { id: 3, content: "Frikandel/Kipcorn/Bamischijf", price: 1.2 }
         ]
     },
     {
         id: 3,
-        date: "Zondag 31/15",
+        date: "Maandag 29/4",
         cateringelements: [
-          { id: 1, content: "scs lekkerezddfdsvfvdrfveredf", price: 25 },
-          { id: 2, content: "cvdfgb", price: 45 },
-          { id: 3, content: "cdv", price: 59 }
+          { id: 1, content: "Croque monsieur", price: 1 },
+          { id: 2, content: "Panini", price: 1.5 },
+          { id: 3, content: "Paella", price: 4 }
         ]
     },
     {
         id: 4,
-        date: "Zondag 31/15",
+        date: "Dinsdag 30/4",
         cateringelements: [
-          { id: 1, content: "scs", price: 25 },
-          { id: 2, content: "cvdfgb", price: 45 },
-          { id: 3, content: "cdv", price: 59 }
+          { id: 1, content: "Brunch", price: 4 },
+          { id: 2, content: "Chili con carne", price: 4 },
+          { id: 3, content: "Chili sin carne", price: 4 }
         ]
     },
     {
         id: 5,
-        date: "Zondag 31/15",
+        date: "Woensdag 1/5",
         cateringelements: [
-          { id: 1, content: "scs", price: 25 },
-          { id: 2, content: "cvdfgb", price: 45 },
-          { id: 3, content: "cdv", price: 59 }
-        ]
-    },
-    {
-        id: 6,
-        date: "Zondag 31/15",
-        cateringelements: [
-          { id: 1, content: "scs", price: 25 },
-          { id: 2, content: "cvdfgb", price: 45 },
-          { id: 3, content: "cdv", price: 59 }
+          { id: 1, content: "Wraps", price: 2.5 },
+          { id: 2, content: "BBQ: 2 stukken vlees", price: 4 },
+          { id: 3, content: "BBQ: 3 stukken vlees", price: 5 }
         ]
     },
 ];
