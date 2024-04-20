8a653a3957c89af9347c2ba4ec923e63ca90b84f
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-04-20 22:55:50 +0200

    add logos

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -12,10 +12,9 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+        className="relative w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
       >
-        <Image src={imageLight} alt={name} fill className="hidden dark:block" />
-        <Image src={image} alt={name} fill className="block dark:hidden" />
+        <Image src={image} alt={name} width={500} height={500} className="block max-w-200 max-h-100 m-4" />
       </a>
     </div>
   );
