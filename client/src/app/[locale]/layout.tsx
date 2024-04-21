9d7a993af5a14a18d7f437857acadbd3a3e09201
Tiddo Nees <tiddo.nees@gmail.com>
2024-04-21 21:51:30 +0200

    Google index, fourth attempt

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -1,9 +1,7 @@
-"use client";
 
 // For monitoring speed on vercel deployment
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-import { useEffect } from 'react';
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
@@ -11,6 +9,12 @@ import { Inter } from "next/font/google";
 import { Libre_Franklin } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "@/styles/index.css";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  verification: { google: "Nav6TjWrom0MGOZw-Ai5X-UFSxfMHMfifS1BMU38ge0" },
+}
 
 const inter = Inter({ subsets: ["latin"] });
 const libreFranklin = Libre_Franklin({
