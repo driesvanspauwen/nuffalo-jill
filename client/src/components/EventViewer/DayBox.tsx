0065355a2f28881603068ff4a27d1e79b12f9689
Maarten <104211647+MaartenHaine@users.noreply.github.com>
2024-04-21 21:29:01 +0200

    fixed shadow around borders

--- client/src/components/EventViewer/DayBox.tsx
+++ client/src/components/EventViewer/DayBox.tsx
@@ -13,7 +13,7 @@ const DayBox = (props: { packageName: string; children: React.ReactNode }) => {
 
   return (
       <div className="w-full">
-        <div className="relative z-10 bg-brown-dark px-8 py-10 mt-10 shadow-three hover:shadow-one dark:shadow-two dark:hover:shadow-gray-dark rounded-lg">
+        <div className="relative z-10 bg-brown-dark px-8 py-10 mt-10 shadow-three hover:shadow-gray-dark rounded-lg">
           <div className="flex items-center justify-between text-nowrap" onClick={toggleCollapse}>
             <SectionTitle
                 title={packageName}
