84b846f69e44c2dbcd642e5e8f980a032461026e
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-04-25 09:33:45 +0200

    Added Westerncantus event

--- client/src/components/EventViewer/index.tsx
+++ client/src/components/EventViewer/index.tsx
@@ -36,8 +36,8 @@ const Schedule = ({title, dag1, dag2, dag3, dag4}) => {
             </DayBox>
             <DayBox packageName={dag3}>
               <div className="mb-8">
-                {/*<EventBanner link="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D"
-                             image="/images/banners/Fakfeestje.png"></EventBanner>*/}
+                <EventBanner link="https://fb.me/e/3GvhzAU7n"
+                             image="/images/banners/Westerncantus.jpg"></EventBanner>
               </div>
             </DayBox>
             <DayBox packageName={dag4}>
